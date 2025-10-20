0e96907aaa8773710271a18dcb163cab0a4e68ad
MartBent <martbent99@gmail.com>
2025-10-20 19:07:43 +0200

    Add Cloudflare DNS record and route for drive service

--- infra/main.tf
+++ infra/main.tf
@@ -34,6 +34,14 @@ resource "cloudflare_dns_record" "n8n_record" {
   proxied = true
 }
 
+resource "cloudflare_dns_record" "drive_record" {
+  zone_id = data.cloudflare_zones.this.result[0].id
+  name    = var.drive_prefix
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.this.id}.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
 // create application routes for tunnel
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "this" {
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.this.id
@@ -47,6 +55,13 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "this" {
       {
         hostname = "${var.n8n_prefix}.${var.cloudflare_domain_name}"
         service  = "http://${var.host_local_ip}:5678"
+      },
+      {
+        hostname = "${var.drive_prefix}.${var.cloudflare_domain_name}"
+        service  = "https://${var.host_local_ip}:5001"
+        origin_request = {
+          no_tls_verify = true
+        }
       },
       {
         service = "http_status:404"
